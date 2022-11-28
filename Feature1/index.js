178ec7c33f87464f315de6d676c3436864e08936
kevit-milan-solanki <milan.solanki.kevit@gmail.com>
2022-11-28 12:43:08 +0530

    5

--- Feature1/index.js
+++ Feature1/index.js
@@ -50,8 +50,3 @@ app.get('/', function (req, res) {
     res.send(`Hello GET. Users are: ${userCount}`);
  })
 
- app.get('/', function (req, res) {
-    console.log("Got a GET request for the homepage");
-    const userCount = 10;
-    res.send(`Hello GET. Users are: ${userCount}`);
- })
